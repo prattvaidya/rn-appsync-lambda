d723aa341f1141fa96b95f327f06d61e4cdc80a8
Pratt Vaidya <pratt.vaidya@gmail.com>
2021-08-03 13:42:16 -0700

    setup datastore with lambda part 1

---
+++ base-models/index.d.ts
@@ -0,0 +1,38 @@
+import { ModelInit, MutableModel, PersistentModelConstructor } from "@aws-amplify/datastore";
+
+export enum PostStatus {
+  DRAFT = "draft",
+  PUBLISHED = "published"
+}
+
+
+
+type PostMetaData = {
+  readOnlyFields: 'createdAt' | 'updatedAt';
+}
+
+type CommentMetaData = {
+  readOnlyFields: 'createdAt' | 'updatedAt';
+}
+
+export declare class Post {
+  readonly id: string;
+  readonly title: string;
+  readonly content?: string;
+  readonly post_status: PostStatus | keyof typeof PostStatus;
+  readonly comments?: (Comment | null)[];
+  readonly createdAt?: string;
+  readonly updatedAt?: string;
+  constructor(init: ModelInit<Post, PostMetaData>);
+  static copyOf(source: Post, mutator: (draft: MutableModel<Post, PostMetaData>) => MutableModel<Post, PostMetaData> | void): Post;
+}
+
+export declare class Comment {
+  readonly id: string;
+  readonly content: string;
+  readonly post?: Post;
+  readonly createdAt?: string;
+  readonly updatedAt?: string;
+  constructor(init: ModelInit<Comment, CommentMetaData>);
+  static copyOf(source: Comment, mutator: (draft: MutableModel<Comment, CommentMetaData>) => MutableModel<Comment, CommentMetaData> | void): Comment;
+}
